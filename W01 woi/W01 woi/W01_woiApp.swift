e6aa61bfe0991677eb8a137fefa40f7afd3a1af8
hsutedjo <161410083+hsutedjo@users.noreply.github.com>
2025-09-11 13:05:37 +0700

    W1 Notes and Trial

---
+++ W01 woi/W01 woi/W01_woiApp.swift
@@ -0,0 +1,17 @@
+//
+//  W01_woiApp.swift
+//  W01 woi
+//
+//  Created by student on 11/09/25.
+//
+
+import SwiftUI
+
+@main
+struct W01_woiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ W01 woi/W01 woiTests/W01_woiTests.swift
@@ -0,0 +1,17 @@
+//
+//  W01_woiTests.swift
+//  W01 woiTests
+//
+//  Created by student on 11/09/25.
+//
+
+import Testing
+@testable import W01_woi
+
+struct W01_woiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
